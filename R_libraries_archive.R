af7fe6167f1dc3342a27cb28e95b3ea8d740d413
andersfi <anders.finstad@ntnu.no>
2015-05-27 08:59:14 +0200

    My R libraries
    Convinience archive of R packages

---
+++ R_libraries_archive.R
@@ -0,0 +1,55 @@
+#########################################################
+# installing libraries when renistalling R ##############
+########################################################
+
+## data management 
+install.packages("abind",dependencies=T) 
+install.packages("chron",dependencies=T) # date and time fixing
+install.packages("RODBC",dependencies=T) 
+install.packages("plyr",dependencies=T) 
+install.packages("dplyr",dependencies=T) 
+install.packages("data.table",dependencies=T) 
+install.packages("RPostgreSQL",dependencies=T) 
+install.packages("xlsx",dependencies=T) 
+install.packages("rJava",dependencies=T) 
+install.packages("caTools",dependencies=T)
+install.packages("knitr",dependencies=T)
+
+
+## geostatistcs and mapdata 
+install.packages("raster",dependencies=T)
+install.packages("sp",dependencies=T)
+install.packages("gstat",dependencies=T)
+install.packages("rgeos",dependencies=T)
+install.packages("rgdal",dependencies=T)
+install.packages("ggmap",dependencies=T)
+install.packages("mapproj",dependencies=T)
+install.packages("geoR",dependencies=T)
+install.packages("pgirmess",dependencies=T)
+install.packages("maps",dependencies=T)
+install.packages("maptools",dependencies=T)
+install.packages("mapdata",dependencies=T)
+install.packages("leaflet",dependencies=T)
+
+# bayesian
+install.packages("R2jags",dependencies=T)
+
+# mixed models 
+install.packages("lme4",dependencies=T)
+install.packages("nlme",dependencies=T)
+install.packages("mgcv",dependencies=T)
+install.packages("gamm4",dependencies=T)
+
+# model selection
+install.packages("MASS",dependencies=T)
+install.packages("MuMIn",dependencies=T)
+install.packages("AICcmodavg",dependencies=T)
+install.packages("relaimpo",dependencies=T)
+
+
+# various  packagers 
+install.packages("rkt",dependencies=T)
+install.packages("animation",dependencies=T)
+install.packages("jpeg",dependencies=T)
+
+
